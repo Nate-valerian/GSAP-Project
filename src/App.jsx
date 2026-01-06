e3d1d4ea3efe67782de0b4afdf5f7443d5b00468
Nate Valerian <n.nate1544@gmail.com>
2026-01-07 01:21:16 +0300

    first GSAP project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <h1 className="text-5xl text-violet-300 font-bold font-circular-web">
+        welcome to my app
+      </h1>
+    </div>
+  );
+};
+
+export default App;
